9f307e26ccad724ec991214e53908bb858ba6642
AntixxTechHub <info@antixxtechhub.com>
2023-10-29 00:47:38 +0530

    final form

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
-const port =process.env.PORT || 5001;
+const port = process.env.PORT || 5001;
 
-const corsOptions ={
-  origin:['http://localhost:3000', 'https://antixxtechhub.com'], 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://antixxtechhub.com'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
 }
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({
+  limit: "50mb"
+}))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -53,16 +55,18 @@ app.use(bodyParser.json());
 // };
 
 // Example usage
-app.post('/send-email', async(req, res) => {
+app.post('/send-email', async (req, res) => {
   try {
-    const { email,
+    const {
+      email,
       name,
       number,
       experience,
       skillSet,
       description,
-      application ,
-      file} = req.body;
+      application,
+      fileName,
+      file } = req.body;
 
     const transporter = nodemailer.createTransport({
       port: 465,
@@ -72,8 +76,8 @@ app.post('/send-email', async(req, res) => {
         pass: "2]6!NK{lo3SJ",
       },
       secure: true,
-      tls: {rejectUnauthorized: false},
-      debug:true
+      tls: { rejectUnauthorized: false },
+      debug: true
     });
 
     const mailData = {
@@ -81,7 +85,11 @@ app.post('/send-email', async(req, res) => {
       to: "careers@antixxtechhub.com",
       subject: `Application for ${application}`,
       html: `<p>Hello,</p> <p>You received a new application from <b>${name}</b> for the <b>${application} </b>position. </p><p ><b>Name :</b>${name}<br><b>Email :</b>${email}<br><b>Phone Number :</b> ${number}<br><b>Experience:</b> ${experience}<br><b>Skills:</b> ${skillSet}<br><b>Description:</b> ${description}<br> </p><p>Best wishes,<br>Team Antixx TechHub</p>`,
-      attachments: file
+      attachments: [{
+        filename: fileName,
+        content: file,
+        encoding: 'base64'
+      }],
     };
     await new Promise((resolve, reject) => {
       transporter.sendMail(mailData, (err, info) => {
@@ -90,16 +98,13 @@ app.post('/send-email', async(req, res) => {
           reject(err);
         } else {
           resolve(info);
-          res.status(200).json({ message: "Email sent" ,info});
+          res.status(200).json({ message: "Email sent", info });
         }
       });
     });
-
-
-
   } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.listen(port, () => {
