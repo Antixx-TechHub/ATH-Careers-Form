86a93eab76da38bab220bb2e695c5c9987b8b4a8
AntixxTechHub <info@antixxtechhub.com>
2023-10-31 01:51:47 +0530

    contact us

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ app.use(express.json({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const transporter = nodemailer.createTransport({
+  port: 465,
+  host: "mail.antixxtechhub.com",
+  auth: {
+    user: "careers@antixxtechhub.com",
+    pass: "2]6!NK{lo3SJ",
+  },
+  secure: true,
+  tls: { rejectUnauthorized: false },
+  debug: true
+});
+
 app.post('/send-email', async (req, res) => {
   try {
     const {
@@ -32,18 +44,6 @@ app.post('/send-email', async (req, res) => {
       fileName,
       file } = req.body;
 
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "mail.antixxtechhub.com",
-      auth: {
-        user: "careers@antixxtechhub.com",
-        pass: "2]6!NK{lo3SJ",
-      },
-      secure: true,
-      tls: { rejectUnauthorized: false },
-      debug: true
-    });
-
     const mailData = {
       from: email,
       to: "careers@antixxtechhub.com",
@@ -54,6 +54,39 @@ app.post('/send-email', async (req, res) => {
         content: file,
         encoding: 'base64'
       }],
+    };
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(info);
+          res.status(200).json({ message: "Email sent", info });
+        }
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post('/send-contact-email', async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      phone_number,
+      msg_subject,
+       country,
+      companyName,
+      message } = req.body;
+
+    const mailData = {
+      from: email,
+      to: "info@antixxtechhub.com",
+      subject: `${msg_subject}`,
+      html: `<p>Hello,</p> <p>You received a new application from <b>${name}</b> for the <b>${companyName} </b>position. </p>${msg_subject}<p ><b>Name :</b>${name}<br><b>Email :</b>${email}<br><b>Phone Number :</b> ${phone_number}<br><b>Country:</b> ${country}<br><b>Company Name:</b> ${companyName}<br><b>Message:</b> ${message}<br> </p><p>Best wishes,<br>Team Antixx TechHub</p>`,
     };
     await new Promise((resolve, reject) => {
       transporter.sendMail(mailData, (err, info) => {
